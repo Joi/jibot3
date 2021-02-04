b101bd582ce4112bb9c61f601d1aa5e7362c9260
Margaret Nottingham <peg@themnigroup.com>
2021-02-04 15:08:06 -0500

    missed in prev commit.

--- server.ts
+++ server.ts
@@ -1,38 +1,27 @@
 import 'zone.js/dist/zone-node';
-import { enableProdMode } from  '@angular/core';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
-
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
-
 let distFolder = process.cwd();
 export function app(): express.Express {
 	const server = express();
 	const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
-	// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 	server.engine('html', ngExpressEngine({
 		bootstrap: AppServerModule,
 	}));
 	server.set('view engine', 'html');
 	server.set('views', `${distFolder}/browser`);
-
-	// Example Express Rest API endpoints
-	// server.get('/api/**', (req, res) => { });
-	// Serve static files from /browser
 	server.get('*.*', express.static(distFolder, {
 		maxAge: '1y'
-	}));
-	// All regular routes use the Universal engine
+	}));e
 	server.get('*', (req, res) => {
 		res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
 	});
-
 	return server;
 }
-
 function run(): void {
 	const port = process.env.PORT || 4000;
 	const server = app();
@@ -40,10 +29,6 @@ function run(): void {
 		console.log(`Node Express server listening on http://localhost:${port}`);
 	});
 }
-
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
