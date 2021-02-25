aedf0ab4f2784acd72970766b4bba9ebe2fa7a75
Margaret Nottingham <peg@themnigroup.com>
2021-02-25 12:17:30 -0500

    add components skeleton

---
+++ src/app/components/apis/edit-api/edit-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-api',
+  templateUrl: './edit-api.component.html',
+  styleUrls: ['./edit-api.component.scss']
+})
+export class EditApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
