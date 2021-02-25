701410976b7187e4f6ff3846fc9c0220e94f3863
Margaret Nottingham <peg@themnigroup.com>
2021-02-25 12:18:04 -0500

    update packaging scripts

--- projects/sqlite3/src/public-api.ts
+++ projects/sqlite3/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of sqlite3
  */
 
-export * from './lib/sqlite3.service';
-export * from './lib/sqlite3.component';
+// export * from './lib/sqlite3.service';
+// export * from './lib/sqlite3.component';
 export * from './lib/sqlite3.module';
