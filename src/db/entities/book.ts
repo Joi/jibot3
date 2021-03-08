ca89f344703d329e5b7e84e19d80b4bddd8f7a3f
Margaret Nottingham <peg@themnigroup.com>
2021-03-08 17:36:38 -0500

    remove unused things

--- src/db/entities/book.ts
+++
@@ -1,7 +0,0 @@
-import {Entity, PrimaryGeneratedColumn, Column, AfterLoad} from "typeorm";
-@Entity()
-export class Book {
-	@PrimaryGeneratedColumn() id: number;
-    @Column() title: string;
-    @Column() url: string;
-}
