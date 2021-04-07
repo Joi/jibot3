adddc8fe8d6cc42cd6d2e9043f55a22cc4a2a417
Margaret Nottingham <peg@themnigroup.com>
2021-04-07 09:42:06 -0400

    change callback to lambda func

--- src/app/modules/slack/messages/rot13/rot13.ts
+++ src/app/modules/slack/messages/rot13/rot13.ts
@@ -4,7 +4,7 @@ import * as Bolt from '../../bolt/bolt.interface';
 export class Rot13 implements Bolt.Message {
 	public name =	'rot13';
 	public regex =	/(bot).*/;
-	public async callback({event, say}) {
+	public callback = async ({event, say}) => {
 		let rot13 = (str) => str.split('').map(char => String.fromCharCode(char.charCodeAt(0) + (char.toLowerCase() < 'n' ? 13 : -13))).join('');
 		return await say(rot13(event.text));
 	}
