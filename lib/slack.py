81f712127d388aa86aa91b8d7c5f03645acc623b
pegnott <peg.nottingham@gmail.com>
2021-05-06 17:51:38 -0400

    start of webhook logging

--- lib/slack.py
+++ lib/slack.py
@@ -27,21 +27,25 @@ class app:
 	user_token = os.environ.get("JIBOT_SLACK_USER_TOKEN", None)
 	verification_token = os.environ.get("JIBOT_SLACK_VERIFICATION_TOKEN", None)
 	port = int(os.environ.get("JIBOT_PORT", 3000))
+	webhook_url: str = os.environ.get("JIBOT_SLACK_WEBHOOK_URL", None)
+
 	ngrok_token = os.environ.get("JIBOT_NGROK_AUTH_TOKEN", None)
 	ngrok_hostname = os.environ.get("JIBOT_NGROK_HOSTNAME", None)
 	has_ngrok:bool = True if ngrok_token is not None else False
 	do_socket_mode:bool = ast.literal_eval(os.environ.get("JIBOT_DO_SOCKET_MODE", 'True'))
+
 	api_connected:bool = False
 	bot_user = None
-	user = None
-	users = None
 	channels = None
-	webhook_url: str = "https://hooks.slack.com/services/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"
-	webhook:WebhookClient = WebhookClient(url=webhook_url) if webhook_url is not None else None
-	logging = logging
+	users = None
 
+	webhook:WebhookClient = WebhookClient(webhook_url) if webhook_url is not None else None
+	logging = logging
+	welcome_message:list = []
 	def log_to_slack(self, message, *args, **kwargs):
-		self.webhook.send(text=message)
+		self.logging.info(message)
+		if self.webhook is not None:
+			self.webhook.send(text=message)
 
 	def __init__(self):
 		self.logging.debug(inspect.currentframe().f_code.co_name)
@@ -54,13 +58,13 @@ class app:
 		)
 		logging.Logger.slack = self.log_to_slack
 		self.logging = logging.getLogger(self.bolt.name.upper())
+		self.logging.slack("*Starting jibot...*")
+		self.load_plugins()
 		self.bolt.use(self.global_listener)
 		self.test_slack_client_connection()
 		self.who_is_bot()
-		# self.who_made_me()
-		# self.where_is_bot()
-		# self.what_channels_am_i_on()
-		# self.who_is_here()
+		self.get_slack_info()
+		self.logging.slack("\r".join(self.welcome_message))
 		try:
 			self.start()
 		except KeyboardInterrupt:
@@ -77,12 +81,12 @@ class app:
 		logging.error(error.response)
 
 	def load_plugins(self):
-		self.logging.debug(inspect.currentframe().f_code.co_name)
-		self.logging.slack("Loading slack plugins...")
+		self.welcome_message.append(f"Loading slack plugins...")
 		app_dir = os.getcwd()
 		plugins_dir = app_dir + os.sep +  'plugins'
 		plugin_files = glob.glob(plugins_dir + os.sep + "**" + os.sep + "[!__]*.py", recursive=True)
 		path_regex = re.compile("^plugins\/(\w+)\/(.+)\.py$")
+		log_message:list = []
 		for plugin_path in plugin_files:
 			relative_path = os.path.relpath(plugin_path, os.getcwd())
 			matches = path_regex.match(relative_path)
@@ -93,12 +97,12 @@ class app:
 					"type": matches.group(1),
 					"name": matches.group(2),
 				}
-				self.logging.slack(f"Type: {plugin.get('type')}, keyword: {plugin.get('keyword')}")
 				plugin['lib'] = importlib.import_module(plugin.get('import_path'))
 				keyword = plugin.get('name')
+				log_message.append(f"{plugin.get('type')}, keyword: {keyword}")
 				try:
 					keyword = plugin.get('lib').keyword
-					self.logging.slack(f"This plugin has a keyword specified ('{keyword}'), this overrides using the plugin filename as the keyword.")
+					log_message.append(f"This plugin has a keyword specified ('{keyword}'), this overrides using the plugin filename as the keyword.")
 				except AttributeError as e:
 					assert(e)
 				if plugin.get('type') == 'command' and keyword.startswith('/') is not True:
@@ -106,24 +110,25 @@ class app:
 				callback_function:callable = plugin.get('lib').callback_function
 				handler_function:callable = getattr(self.bolt, plugin.get('type'))
 				handler_function(keyword)(callback_function)
+		self.welcome_message.append("\r\t".join(log_message))
 
 	def start(self):
 		self.logging.debug(inspect.currentframe().f_code.co_name)
 		if self.do_socket_mode is True:
 			self.socket_mode = SocketModeHandler(self.bolt, self.app_token)
-			self.logging.slack("Starting bolt app in Socket mode....")
+			self.welcome_message.append("Starting bolt app in Socket mode....")
 			self.socket_mode.start()
 
 		else:
 			if (self.has_ngrok is True):
-				self.logging.slack("Detected ngrok... starting request url tunnel...")
+				self.welcome_message.append("Detected ngrok... starting request url tunnel...")
 				request_url_tunnel = ngrok.connect(self.port, "http", subdomain=self.ngrok_hostname)
 				ngrok_process = ngrok.get_ngrok_process()
-				self.logging.slack("Request Url is: " + request_url_tunnel.public_url)
+				self.welcome_message.append("Request Url is: " + request_url_tunnel.public_url)
 			self.bolt.start(port=self.port)
 
 	def close(self):
-		self.logging.slack("Shutting jibot down...")
+		self.logging.slack("*Shutting jibot down...*")
 		self.logging.debug(inspect.currentframe().f_code.co_name)
 		if (self.has_ngrok is True):
 			ngrok.kill()
@@ -132,7 +137,7 @@ class app:
 		self.logging.debug(inspect.currentframe().f_code.co_name)
 		try:
 			self.api_connected = self.bolt.client.api_test().get("ok")
-			self.logging.slack("Slack web client connection established...")
+			self.welcome_message.append("Slack web client connection established...")
 		except SlackApiError as e:
 			self.logging.error("Unable to establish a slack web client connection!")
 			self.slack_api_error(e)
@@ -142,64 +147,39 @@ class app:
 		try:
 			bot_auth = self.bolt.client.auth_test(token=self.bot_token)
 			self.bot_user = self.bolt.client.users_info(user=bot_auth.get("user_id")).get("user")
-			self.logging.slack(f"Starting jibot. Jibot is named '{self.bot_user.get('real_name')}.'")
+			self.welcome_message.append(f"Starting jibot. Jibot is named '{self.bot_user.get('real_name')}.'")
 			self.logging.slack(self.bot_user)
 		except SlackApiError as e:
 			self.slack_api_error(e)
 
-	def where_is_bot(self):
+	def get_slack_info(self):
 		self.logging.debug(inspect.currentframe().f_code.co_name)
 		if self.bot_user is not None:
 			team_id = self.bot_user.get("team_id", None)
 			try:
 				self.team = self.bolt.client.team_info(team=team_id).get("team")
-				self.logging.slack(f"I am on slack team named {self.team.get('name')}.")
-				self.logging.debug(self.team)
-			except SlackApiError as e:
-				self.slack_api_error(e)
-		try:
-			self.channels = self.bolt.client.conversations_list().get('channels')
-			self.logging.slack(f"There are {len(self.channels)} channels.")
-		except SlackApiError as e:
-			self.slack_api_error(e)
-
-	def who_is_here(self):
-		self.logging.debug(inspect.currentframe().f_code.co_name)
-		try:
-			self.users = self.bolt.client.users_list().get('members')
-			self.logging.slack(f"There are {len(self.users)} users.")
-			self.logging.debug(self.users)
-		except SlackApiError as e:
-			self.slack_api_error(e)
-
-	def who_made_me(self):
-		self.logging.debug(inspect.currentframe().f_code.co_name)
-		if (self.user_token is not None):
-			try:
-				user_auth = self.bolt.client.auth_test(token=self.user_token)
-				self.user = self.bolt.client.users_info(user=user_auth.get("user_id")).get("user")
-				self.logging.slack(f"I was installed here by {self.user.get('real_name')}.")
-				self.logging.slack(self.user)
+				self.channels = self.bolt.client.conversations_list().get('channels')
+				self.users = self.bolt.client.users_list().get('members')
 			except SlackApiError as e:
 				self.slack_api_error(e)
 
-	def what_channels_am_i_on(self):
-		self.logging.debug(inspect.currentframe().f_code.co_name)
-		bot_channels:list = []
-		for channel in self.channels:
-			try:
-				channel_members =  self.bolt.client.conversations_members(channel=channel.get('id')).get('members')
-				if self.bot_user is not None and self.bot_user.get('id') in channel_members:
-					bot_channels.append(channel.get('id'))
-					self.logging.slack(f"I am on #{channel.get('name')}. I should say hello.")
-					self.bolt.client.chat_postMessage(
-						channel=channel.get('id'),
-						text=f"Hello #{channel.get('name')}! I am {self.bot_user.get('real_name')}. I am waking up."
-					)
-			except SlackApiError as e:
-				self.slack_api_error(e)
-		if len(bot_channels) == 0:
-			self.logging.warning("The bot is NOT on any slack channels. Should we consider having the bot create a channel (if scopes allow)? You can also add bot to a channel via @mention_bot_name")
+	# def what_channels_am_i_on(self):
+	# 	self.logging.debug(inspect.currentframe().f_code.co_name)
+	# 	bot_channels:list = []
+	# 	for channel in self.channels:
+	# 		try:
+	# 			channel_members =  self.bolt.client.conversations_members(channel=channel.get('id')).get('members')
+	# 			if self.bot_user is not None and self.bot_user.get('id') in channel_members:
+	# 				bot_channels.append(channel.get('id'))
+	# 				self.logging.slack(f"I am on #{channel.get('name')}. I should say hello.")
+	# 				self.bolt.client.chat_postMessage(
+	# 					channel=channel.get('id'),
+	# 					text=f"Hello #{channel.get('name')}! I am {self.bot_user.get('real_name')}. I am waking up."
+	# 				)
+	# 		except SlackApiError as e:
+	# 			self.slack_api_error(e)
+	# 	if len(bot_channels) == 0:
+	# 		self.logging.warning("The bot is NOT on any slack channels. Should we consider having the bot create a channel (if scopes allow)? You can also add bot to a channel via @mention_bot_name")
 
 	def global_listener(self, ack, action, client, command, context, event, logger, message, next, options, payload, request, response, respond, say, shortcut, view):
 		logger.info("+++++++" + inspect.currentframe().f_code.co_name + "+++++++")
