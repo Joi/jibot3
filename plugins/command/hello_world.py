32d7d5921bc82ace47a0c0dd7b2714eda8a04001
pegnott <peg.nottingham@gmail.com>
2021-05-06 15:43:02 -0400

    user filename as keyword

--- plugins/command/hello_world.py
+++ plugins/command/hello_world.py
@@ -1,5 +1,2 @@
-keyword="/hello_world"
 def callback_function(say, command):
-	# print(command)
-	# # # # (and you, <@{user}>)
 	say(f"HELLO WORLD! This command is running from: {__file__}")
--- plugins/message/hello_world.py
+++ plugins/message/hello_world.py
@@ -1,4 +1,3 @@
-keyword="hello world"
 def callback_function(message, say):
 	user = message['user']
 	say(f"HELLO WORLD (and you, <@{user}>)! This code is running from: {__file__}")
