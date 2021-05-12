611593b0e68364e616e94099a2dc6e72ca0fc86b
pegnott <peg.nottingham@gmail.com>
2021-05-12 11:32:50 -0400

    server start

---
+++ web/hello_world.py
@@ -0,0 +1,10 @@
+# import logging
+# logging.info("ASLKDJASLKJDLKASJDLKJASLDKJSA")
+# logging.info("ASLKDJASLKJDLKASJDLKJASLDKJSA")
+# logging.info("ASLKDJASLKJDLKASJDLKJASLDKJSA")
+# logging.info("ASLKDJASLKJDLKASJDLKJASLDKJSA")
+# logging.info("ASLKDJASLKJDLKASJDLKJASLDKJSA")
+# # import cgitb
+# # cgitb.enable()
+# # form = cgi.FieldStorage()
+# # print(form)
