f6686e318f638c6378d3581a79007c2c5241be41
pegnott <peg.nottingham@gmail.com>
2021-05-13 10:26:02 -0400

    shortcuts

---
+++ plugins/shortcut/hello_world.py
@@ -0,0 +1,30 @@
+def callback_function(ack, client, logger, payload, shortcut, say,):
+	user = payload['user']
+	ack()
+	client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        # A simple view payload for a modal
+        view={
+            "type": "modal",
+            "title": {"type": "plain_text", "text": "Hello World!"},
+            "close": {"type": "plain_text", "text": "Close"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hello world!"
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "I am jibot app."
+                        }
+                    ]
+                }
+            ]
+        }
+    )
