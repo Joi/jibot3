b36b09ab558994182a1aa93487e968ab8a5df5de
pegnott <peg.nottingham@gmail.com>
2021-05-17 11:13:42 -0400

    create sqlite.db if it doesn't exist

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-
-import lib.slack as bolt
 import logging
+import os
+import lib.slack as bolt
+
 logging.basicConfig(
 	level=logging.INFO,
 	format='%(levelname)s:%(message)s'
 )
+def init_db():
+	db_file = f"{os.getcwd()}{os.sep}sqlite.db"
+	if os.path.exists(db_file):
+		logging.debug(f"Database file exists... {db_file}")
+	else:
+		logging.debug(f"Database file does not exist... creating {db_file}")
+		try:
+			os.mknod(db_file)
+		except:
+			with open(db_file, 'w'): pass
+
 def main():
-	try:
-		bolt.app()
-	except KeyboardInterrupt:
-		bolt.app.close()
+	init_db()
+	bolt.app()
 
 if __name__ == "__main__":
 	main()
