db3a8b3f9a6969fd69d5851a7e5f126b1dab89fc
pegnott <peg.nottingham@gmail.com>
2021-05-24 10:32:57 -0400

    sets definition / no unset yet

--- plugins/message/definition.py
+++ plugins/message/definition.py
@@ -1,29 +1,51 @@
+import	inspect
+import json
 import logging
 from pathlib import Path
 import re
-import sqlite3
 from lib.database import SQLite
 
 spaces:str = "|".join([' ', '\xa0'])
 space_re = f"({spaces})+"
 
 user_re:str = "<@(?P<user_id>[A-Z0-9]+)>"
-thing_re:str = "(?P<thing>\w+)"
+object_re:str = "(?P<object>\w+)"
 operator_re:str = "(?P<operator>is not|is)"
-att_re:str = "(?P<attribute>\w+)"
-keyword:re = re.compile(f"({user_re}|{thing_re}){space_re}{operator_re}{space_re}{att_re}")
+definition_re:str = "(?P<definition>\w+)"
+keyword:re = re.compile(f"({user_re}|{object_re}){space_re}{operator_re}{space_re}{definition_re}")
+table_name = Path(__file__).stem
+db = None
+
+def _select(key:str = None):
+	logging.debug(inspect.currentframe().f_code.co_name)
+	if key is None:
+		query:str = f"SELECT KEY, json(DEFS) FROM {table_name}"
+		return db.cursor.execute(query, [key]).fetchall()
+	else:
+		query:str = f"SELECT json(DEFS) FROM {table_name} WHERE key=?"
+		values = db.cursor.execute(query, [key]).fetchone()
+		if type(values) == type(tuple()):
+			return json.loads(values[0])
+
+def _define(key, value):
+	logging.debug(inspect.currentframe().f_code.co_name)
+	db.cursor.execute(f"INSERT OR REPLACE INTO {table_name} VALUES(?, json(?))", (key, json.dumps(value)))
+	db.connection.commit()
 
 def callback_function(client, context, logger:logging.Logger, next, payload, request, say):
-	global keyword
-	# db = SQLite()
-	# table_name = Path(__file__).stem
-	# db.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (key text PRIMARY KEY, PLUS int DEFAULT 0, MINUS int DEFAULT 0);")
+	global db, keyword, table_name
+	db = SQLite()
+	db.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (key text PRIMARY KEY, DEFS json);")
 	text = payload.get('text')
 	matches = re.finditer(keyword, text)
 	for match in matches:
 		user_id = match.group('user_id')
-		thing = match.group('thing')
+		object = match.group('object')
 		operator = match.group('operator')
-		attribute = match.group('attribute')
-		object = user_id if user_id is not None else thing
-		logger.info(f"{object} {operator} {attribute}")
+		definition = match.group('definition')
+		object = user_id if user_id is not None else object
+		existing_definitions = _select(object)
+		definitions = set(existing_definitions) if existing_definitions is not None else set()
+		definitions.add(definition)
+		_define(object, list(definitions))
+		logger.info(f"{object} {operator} {definition}")
