011c8380d881b692ca61ee57a2a9da2be41cbf47
pegnott <peg.nottingham@gmail.com>
2021-05-24 16:57:07 -0400

    slash command w/kwargs

--- plugins/command/hello_world.py
+++ plugins/command/hello_world.py
@@ -1,3 +1,7 @@
-def callback_function(client, command, context, logger, next, payload, request, response, respond, say):
+def callback_function(**args):
+	ack = args['ack']
+	payload = args['payload']
+	say = args['say']
 	user = payload['user_id']
+	ack()
 	say(f"HELLO WORLD (and you, <@{user}>)! This code is running from: {__file__}")
