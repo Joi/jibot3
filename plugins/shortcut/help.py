16f8b6ec7135e31bc81a9b24887ed2b0513f2bd8
pegnott <peg.nottingham@gmail.com>
2021-05-27 17:54:15 -0400

    generate blocks based on plugin info (incomplete)

--- plugins/shortcut/help.py
+++ plugins/shortcut/help.py
@@ -1,13 +1,37 @@
+import json
 from slack_sdk.web import WebClient
 def callback_function(ack, client:WebClient, logger, shortcut):
 	ack()
-	logger.info(shortcut)
+	view = {
+		"type": "modal",
+		"title": {
+			"type": "plain_text",
+			"text": "Help",
+			"emoji": True
+		},
+		"close": {
+			"type": "plain_text",
+			"text": "Close",
+			"emoji": True
+		},
+		"blocks": []
+	}
+	plugins = shortcut.get('plugins')
+	blocks = view.get('blocks')
+	if plugins is not None:
+		for plugin in plugins:
+			plugin_type = plugin.get('type', None)
+			if plugin_type == "action": continue
+			keyword:str = plugin.get('keyword', None)
+			event_name:str = plugin.get('event_name', '')
+			blocks.append({
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": f"{plugin_type} {event_name} {keyword}"
+				}
+			})
 	view = client.views_open(
         trigger_id=shortcut["trigger_id"],
-		view={
-			"type": "modal",
-            "title": {"type": "plain_text", "text": "Help"},
-            "close": {"type": "plain_text", "text": "Close"},
-            "blocks": []
-        }
+		view=json.dumps(view)
     )
