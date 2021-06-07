9df4335003d3e55d2b31f35395a9838d4314a958
pegnott <peg.nottingham@gmail.com>
2021-06-07 12:04:47 -0400

    start on getting user info for admin/owner things

--- plugins/event/app_home_opened.py
+++ plugins/event/app_home_opened.py
@@ -3,11 +3,22 @@ from lib.database import SQLite
 from include.blocks.karma import get_blocks as get_karma
 from include.blocks.brain import get_blocks as get_brain
 
-def callback_function(ack, event, client, context):
+from slack_bolt.context import BoltContext
+from slack_bolt.context.ack import Ack
+from slack_bolt.context.respond import Respond
+from slack_bolt.context.say import Say
+from slack_bolt.request import BoltRequest
+from slack_bolt.response import BoltResponse
+from slack_sdk import WebClient
+from slack_sdk.web.slack_response import SlackResponse
+
+def callback_function(ack:Ack, body:dict, event:dict, client:WebClient, context: BoltContext, payload:dict, request:BoltRequest):
 	ack()
 	view = event.get('view', None)
 	view_id = view.get('id') if view is not None else None
 	user_id = event["user"]
+	user_response:SlackResponse = client.users_info(user=user_id)
+	user = user_response.get('user') if user_response.get('ok') else None
 	app_home_view = {
 		"type": "home",
 		"blocks": [{
@@ -18,7 +29,39 @@ def callback_function(ack, event, client, context):
 			}
 		}]
 	}
+	# if user.get('is_admin') or user.get('is_owner'):
+	# 	if user.get('is_admin'):
+	# 		app_home_view['blocks'].extend([
+	# 			{
+	# 				"type": "section",
+	# 				"text": {
+	# 					"type": "plain_text",
+	# 					"text": ":wave: Hello operator!",
+	# 					"emoji": True
+	# 				}
+	# 			},
+	# 		])
+	# 	if user.get('is_owner'):
+	# 		app_home_view['blocks'].extend([
+	# 			{
+	# 				"type": "section",
+	# 				"text": {
+	# 					"type": "plain_text",
+	# 					"text": ":wave: Hello owner!",
+	# 					"emoji": True
+	# 				}
+	# 			},
+	# 		])
+	app_home_view['blocks'].append({
+		"type": "header",
+		"text": { "type": "plain_text", "text": ":yin_yang: Karma" }
+	})
 	app_home_view['blocks'].extend(get_karma())
+
+	app_home_view['blocks'].append({
+		"type": "header",
+		"text": { "type": "plain_text", "text": ":brain: Brains" }
+	})
 	app_home_view['blocks'].extend(get_brain())
 	if view_id is None: client.views_publish(user_id=context.get('user_id'),view=json.dumps(app_home_view))
 	else: client.views_update(view_id=view.get('id'), view=json.dumps(app_home_view))
