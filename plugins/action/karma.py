fa0aa651f68876f1a189abd372374f74e924ac24
pegnott <peg.nottingham@gmail.com>
2021-06-08 16:37:48 -0400

    move blocks into main includes

--- plugins/action/karma.py
+++ plugins/action/karma.py
@@ -1,7 +1,7 @@
 import logging
 from slack_bolt import Ack, BoltRequest
 from slack_sdk.web import WebClient
-from include.blocks.karma import get_blocks as get_karma
+import include.karma as karma
 
 def callback_function(ack:Ack, client:WebClient, logger:logging.Logger, request:BoltRequest):
 	container = request.body.get('container', None)
@@ -11,7 +11,7 @@ def callback_function(ack:Ack, client:WebClient, logger:logging.Logger, request:
 	close_button = view.get('close')
 	close_button.update(text="Go Back")
 	content:list = list()
-	content.extend(get_karma())
+	content.extend(karma.blocks())
 	client.views_push(
 		trigger_id=request.body.get('trigger_id'),
 		view={
