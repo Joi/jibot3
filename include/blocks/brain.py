9c5d5b2a0707d7dead983677a9873f3135aceedd
pegnott <peg.nottingham@gmail.com>
2021-06-08 16:08:21 -0400

    rename vars

--- include/blocks/brain.py
+++ include/blocks/brain.py
@@ -2,11 +2,11 @@ import json
 from lib.database import SQLite
 def get_blocks():
 	blocks:list = []
-	_db:SQLite = SQLite()
-	_brain_query:str = "SELECT * FROM definition ORDER BY key ASC;"
-	_brain = _db.cursor.execute(_brain_query).fetchall()
-	if _brain is not None:
-		for b in _brain:
+	db:SQLite = SQLite()
+	brain_query:str = "SELECT * FROM definition ORDER BY key ASC;"
+	brain = db.cursor.execute(brain_query).fetchall()
+	if brain is not None:
+		for b in brain:
 			thing:str = b[0]
 			definitions = json.loads(b[1])
 			blocks.append({
