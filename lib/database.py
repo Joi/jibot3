3935e404fd51e168572845adce8277e51100572d
pegnott <peg.nottingham@gmail.com>
2021-06-08 16:26:54 -0400

    funs to check for and create tables

--- lib/database.py
+++ lib/database.py
@@ -1,6 +1,7 @@
 import logging
 import sqlite3
 from sqlite3 import Connection, Cursor, Error
+
 class SQLite():
 	connection:Connection = None
 	cursor:Cursor = None
@@ -14,3 +15,19 @@ class SQLite():
 			logging.debug(sqlite3.version)
 		except Error as e:
 			logging.error(e)
+
+def get_table(table_name):
+	db:SQLite = SQLite()
+	return db.cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+
+def create_table(table_name:str, table_details:str):
+	db:SQLite = SQLite()
+	db.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} {table_details};")
+
+def db_query(table_name, **kwargs):
+	distinct = "DISTINCT" if kwargs.get('distinct', False) is True else ""
+	columns = kwargs.get('columns', "*")
+	order_by = kwargs.get('order_by', "key")
+	order = kwargs.get('order', "ASC")
+	where = kwargs.get('where', "")
+	return f"SELECT {distinct} {columns} FROM {table_name} {where} ORDER BY {order_by} {order};"
