c0aedfa08a2da1cd2c5c7180d584a17b7d035290
pegnott <peg.nottingham@gmail.com>
2021-06-09 10:43:44 -0400

    add different __doc__ for diff listener types

--- plugins/command/wikipedia.py
+++ plugins/command/wikipedia.py
@@ -1,1 +1,3 @@
 from include.wikipedia import callback_function
+from lib.slack import slack_bot_slash_command as bot_slash_command
+callback_function.__doc__ = f"Private Wikipedia lookup via slash command `/{bot_slash_command} wikipedia <search term or phrase>`"
--- plugins/event/app_mention/wikipedia.py
+++ plugins/event/app_mention/wikipedia.py
@@ -1,1 +1,2 @@
 from include.wikipedia import callback_function
+callback_function.__doc__ = f"Public Wikipedia lookup via bot `@mention wikipedia <search term or phrase>`"
