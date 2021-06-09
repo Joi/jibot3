2d249ce3d0e51c483f647fc388f35ac78186b199
pegnott <peg.nottingham@gmail.com>
2021-06-09 10:58:23 -0400

    adjust to import callback

--- plugins/event/app_mention/wikipedia.py
+++ plugins/event/app_mention/wikipedia.py
@@ -1,2 +1,4 @@
-from include.wikipedia import callback_function
+import include.wikipedia as wikipedia
+
+callback_function:callable =  wikipedia.callback_function
 callback_function.__doc__ = f"Public Wikipedia lookup via bot `@mention wikipedia <search term or phrase>`"
