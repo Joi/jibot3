cbeeeb9ccf63bcc36decd57fe74521fe23ce01f9
pegnott <peg.nottingham@gmail.com>
2021-06-09 10:43:07 -0400

    remove __doc for "invisible" listeners

--- plugins/message/user_likes.py
+++ plugins/message/user_likes.py
@@ -1,3 +1,1 @@
-from include.user_likes import keyword
-from include.user_likes import callback_function
-callback_function.__doc__ = "The bot will watch for phrases indicating that users like things, and will keep track of it."
+from include.user_likes import callback_function, keyword
