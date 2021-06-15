d2675985702f5bb82f5c22e8e178c53a6d816de1
pegnott <peg.nottingham@gmail.com>
2021-06-15 10:01:11 -0400

    db struc change / migration script. move to class.

--- include/definition.py
+++ include/definition.py
@@ -1,47 +1,61 @@
-from lib.database import SQLite, select_query, get_table, create_table
+from lib.database import SQLite, select_query, table_exists, create_table, delete_table, column_exists
+
 import inspect
 import json
 import logging
 import re
+
 from pathlib import Path
+from slack_bolt import Ack, BoltRequest, BoltResponse, Respond, Say
 from stop_words import get_stop_words
 
-spaces:str = "|".join([' ', '\xa0'])
-space_re = f"({spaces})+"
-user_re:str = "<@(?P<user_id>[A-Z0-9]+)>"
-object_re:str = "(?P<object>\w+)"
 
-plus_operators = ["is the", "is", "are"]
-minus_operators = ["is not the", "is not", "isn't", "are not", "aren't"]
-operator_re:str = f"(?P<operator>{'|'.join(minus_operators)}|{'|'.join(plus_operators)})"
-
-definition_re:str = "(?P<definition>\w+)"
-keyword:re = re.compile(f"({user_re}|{object_re}){space_re}{operator_re}{space_re}{definition_re}")
-table_name = Path(__file__).stem
-
-if get_table(table_name).rowcount == -1:
-	create_table(table_name, "key text PRIMARY KEY, DEFS json")
 
 def _select(key:str = None):
 	logging.debug(inspect.currentframe().f_code.co_name)
 	db:SQLite = SQLite()
 	db_response = None
 	if key is None:
-		# query:str = f"SELECT KEY, json(DEFS) FROM {table_name}"
-		query:str = select_query(table_name, columns="KEY, json(DEFS)")
+		query:str = select_query(table_name, columns="OBJECT, ATTRIBUTE")
 		db_response =  db.cursor.execute(query, [key]).fetchall()
 	else:
-		query:str = select_query(table_name, columns="KEY, json(DEFS)", where="WHERE key=?")
-		values = db.cursor.execute(query, [key]).fetchone()
-		if type(values) == type(tuple()):
-			db_response =  json.loads(values[0])
+		query:str = select_query(table_name, columns="OBJECT, ATTRIBUTE", where="WHERE OBJECT=?")
+		db.cursor.execute(query).fetchall()
+	# if type(values) == type(tuple()):
+	# 	db_response =  json.loads(values[0])
 	return db_response
 
-def _define(key, value):
+def _define(object, attribute):
 	logging.debug(inspect.currentframe().f_code.co_name)
 	db:SQLite = SQLite()
-	db.cursor.execute(f"INSERT OR REPLACE INTO {table_name} VALUES(?, json(?))", (key, json.dumps(value)))
+	db.cursor.execute(f"INSERT OR IGNORE INTO {table_name} (OBJECT, ATTRIBUTE) VALUES(?, ?)", (object, attribute))
 	db.connection.commit()
+
+def _undefine(object, attribute):
+	logging.debug(inspect.currentframe().f_code.co_name)
+	db:SQLite = SQLite()
+	db.cursor.execute(f"DELETE FROM {table_name} WHERE OBJECT=? AND ATTRIBUTE=?", (object, attribute))
+	db.connection.commit()
+
+table_schema:str = "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, OBJECT text NOT NULL, ATTRIBUTE text NOT NULL, UNIQUE(OBJECT, ATTRIBUTE)"
+table_name = Path(__file__).stem
+if not table_exists(table_name):
+	create_table(table_name, table_schema)
+else:
+	if column_exists(table_name, "DEFS"):
+		data = _select()
+		tmp_table_name = f"__migrate_{table_name}"
+		db:SQLite = SQLite()
+		db.cursor.execute(f"ALTER TABLE {table_name} RENAME TO {tmp_table_name};")
+		create_table(table_name, table_schema)
+		if data is not None:
+			for d in data:
+				o:str = d[0]
+				atts:list = json.loads(d[1])
+				for att in atts:
+					_define(o, att)
+		if table_exists(table_name) and table_exists(tmp_table_name):
+			delete_table(tmp_table_name)
 
 def blocks():
 	blocks:list = []
@@ -61,22 +75,29 @@ def blocks():
 			})
 	return(blocks)
 
-def callback_function(logger:logging.Logger, payload, request, say):
-	global keyword, table_name, plus_operators, minus_operators
-	text = payload.get('text')
-	matches = re.finditer(keyword, text)
-	for match in matches:
-		user_id = match.group('user_id')
-		object = match.group('object').lower()
-		operator = match.group('operator')
-		definition = match.group('definition')
-		object = user_id if user_id is not None else object
-		if definition not in get_stop_words('english'):
-			existing_definitions = _select(object)
-			definitions = set(existing_definitions) if existing_definitions is not None else set()
-			if operator in plus_operators:
-				definitions.add(definition)
-			elif definition in definitions:
-				definitions.remove(definition)
-			_define(object, list(definitions))
+class message:
+	spaces:str = "|".join([' ', '\xa0'])
+	space_re = f"({spaces})+"
+	user_re:str = "<@(?P<user_id>[A-Z0-9]+)>"
+	object_re:str = "(?P<object>\w+)"
+	plus_operators = ["is the", "is", "are"]
+	minus_operators = ["is not the", "is not", "isn't", "are not", "aren't"]
+	operator_re:str = f"(?P<operator>{'|'.join(minus_operators)}|{'|'.join(plus_operators)})"
+	definition_re:str = "(?P<definition>\w+)"
+	keyword:re = re.compile(f"({user_re}|{object_re}){space_re}{operator_re}{space_re}{definition_re}")
+
+	def __init__(self, logger:logging.Logger, payload:dict, request:BoltRequest, say:Say):
+		matches = re.finditer(self.keyword, payload.get('text'))
+		for match in matches:
+			user_id = match.group('user_id')
+			object = match.group('object').lower()
+			operator = match.group('operator')
+			definition = match.group('definition')
+			object = user_id if user_id is not None else object
 			logger.info(f"{object} {operator} {definition}")
+
+			if definition not in get_stop_words('english'):
+				if operator in self.plus_operators:
+					_define(object, definition)
+				else:
+					_undefine(object, definition)
