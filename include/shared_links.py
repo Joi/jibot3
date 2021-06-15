409b499a14b68d700560177c6eb30d60e7f77b04
pegnott <peg.nottingham@gmail.com>
2021-06-15 10:22:23 -0400

    move to class

--- include/shared_links.py
+++ include/shared_links.py
@@ -1,13 +1,12 @@
-from lib.database import SQLite, select_query, get_table, create_table
+from lib.database import SQLite, select_query, table_exists, create_table
 
 from pathlib import Path
 import logging
 import re
 import sqlite3
 
-keyword:re = re.compile("(\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'\(\),\$_\{\}\^~\[\]`#|]+)", re.IGNORECASE)
 table_name:str = Path(__file__).stem
-if get_table(table_name).rowcount == -1:
+if not table_exists(table_name):
 	create_table(table_name, "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, URL text NOT NULL")
 
 def blocks():
@@ -29,15 +28,16 @@ def blocks():
 			})
 	return(blocks)
 
-def callback_function(logger:logging.Logger, payload:dict):
-	global table_name
-	db:SQLite = SQLite()
-	matches:list = keyword.findall(payload.get('text'))
-	if matches is not None:
-		for url in matches:
-			logger.info(f"Saving link: {url}")
-			try:
-				db.cursor.execute(f"INSERT INTO {table_name} (URL) VALUES(?)", (url,))
-				db.connection.commit()
-			except sqlite3.Error as e:
-				logger.error(e)
+class message:
+	keyword:re = re.compile("(\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'\(\),\$_\{\}\^~\[\]`#|]+)", re.IGNORECASE)
+	def __init__(self, logger:logging.Logger, payload:dict):
+		db:SQLite = SQLite()
+		matches:list = self.keyword.findall(payload.get('text'))
+		if matches is not None:
+			for url in matches:
+				logger.info(f"Saving link: {url}")
+				try:
+					db.cursor.execute(f"INSERT INTO {table_name} (URL) VALUES(?)", (url,))
+					db.connection.commit()
+				except sqlite3.Error as e:
+					logger.error(e)
