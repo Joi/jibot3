64708db5447dbcf0d5208f66939fde5ed853b376
pegnott <peg.nottingham@gmail.com>
2021-07-07 10:33:23 -0400

    better class use

--- plugins/shared_links.py
+++ plugins/shared_links.py
@@ -1,4 +1,4 @@
-from lib.database import SQLite, select_query, table_exists, create_table
+from lib.database import SQLite
 
 from pathlib import Path
 from slack_bolt import Ack, BoltRequest
@@ -8,36 +8,41 @@ import re
 import sqlite3
 
 table_name:str = Path(__file__).stem
-if not table_exists(table_name):
-	create_table(table_name, "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, URL text NOT NULL")
+table_params:str = "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, URL text NOT NULL"
+SQLite().create_table(table_name, table_params)
 
-def blocks():
-	blocks:list = [{
-		"type": "header",
-		"text": {
-			"type": "plain_text",
-			"text": ":link: Shared Links"
-		}
-	}]
-	db:SQLite = SQLite()
-	table_name = Path(__file__).stem
-	shared_links_query:str = f"SELECT * FROM {table_name} ORDER BY URL ASC;"
-	links = db.cursor.execute(shared_links_query).fetchall()
-	if links is not None:
-		for l in links:
-			id:int = l[0]
-			url:str = l[1]
-			blocks.append({
-				"type": "section",
-				"text": {
-					"type": "mrkdwn",
-					"text": url
-				},
-			})
-	return(blocks)
+class shared_links:
+	db:SQLite
+	select_query:str
 
-class action:
+	def __init__(self):
+		self.db = SQLite()
+		self.select_query = self.db.select_query(table_name, order_by="URL", order="ASC")
+
+	def blocks(self):
+		blocks:list = [{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": ":link: Shared Links"
+			}
+		}]
+		links = self.db.cursor.execute(self.select_query).fetchall()
+		if links is not None:
+			for l in links:
+				url:str = l[1]
+				blocks.append({
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": url
+					},
+				})
+		return(blocks)
+
+class action(shared_links):
 	def __init__(self, ack:Ack, client:WebClient, logger:logging.Logger, request:BoltRequest):
+		super().__init__()
 		container = request.body.get('container', None)
 		view:dict = request.body.get(container.get('type'))
 		title:dict = view.get('title')
@@ -45,7 +50,7 @@ class action:
 		close_button = view.get('close')
 		close_button.update(text="Go Back")
 		content:list = list()
-		content.extend(blocks())
+		content.extend(self.blocks())
 		client.views_push(
 			trigger_id=request.body.get('trigger_id'),
 			view={
@@ -56,17 +61,19 @@ class action:
 			}
 		)
 		ack()
-class message:
-	__doc__ = "The bot will save any shared links that is sees. You can view this in the app home."
+
+class message(shared_links):
+	__doc__ = "The bot will save any shared links that is sees."
 	keyword:re = re.compile("(\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'\(\),\$_\{\}\^~\[\]`#|]+)", re.IGNORECASE)
+
 	def __init__(self, logger:logging.Logger, payload:dict):
-		db:SQLite = SQLite()
+		super().__init__()
 		matches:list = self.keyword.findall(payload.get('text'))
 		if matches is not None:
 			for url in matches:
 				logger.info(f"Saving link: {url}")
 				try:
-					db.cursor.execute(f"INSERT INTO {table_name} (URL) VALUES(?)", (url,))
-					db.connection.commit()
+					self.db.cursor.execute(f"INSERT INTO {table_name} (URL) VALUES(?)", (url,))
+					self.db.connection.commit()
 				except sqlite3.Error as e:
 					logger.error(e)
